b9350a25a422ca7d2790bbcef826fe0d0c73e166
tongdexin <120200793@qq.com>
2017-02-21 22:11:40 +0800

    dependent library

--- app/src/main/java/com/tdx/demo/copyassets/MainActivity.java
+++ app/src/main/java/com/tdx/demo/copyassets/MainActivity.java
@@ -3,11 +3,14 @@ package com.tdx.demo.copyassets;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tdx.demo.library.PathUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PathUtil.showAndroidDir();
     }
 }
