3a1329d99db3603907bcc002e2825871f4f1ec58
ohYes <120200793@qq.com>
2017-06-05 20:39:22 +0900

    Update MainActivity.java

--- app/src/main/java/com/tdx/demo/copyassets/MainActivity.java
+++ app/src/main/java/com/tdx/demo/copyassets/MainActivity.java
@@ -14,3 +14,34 @@ public class MainActivity extends AppCompatActivity {
         PathUtil.showAndroidDir();
     }
 }
+SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+				Locale locale = null;
+				if (!prefs.contains("SharedPreferences_language")){
+					if (Locale.JAPANESE.getLanguage().equals(Locale.getDefault().getLanguage())){
+						locale = Locale.JAPANESE;
+					} else if (Locale.CHINESE.getLanguage().equals(Locale.getDefault().getLanguage())){
+						locale = Locale.CHINESE;
+					} else if (Locale.ENGLISH.getLanguage().equals(Locale.getDefault().getLanguage())){
+						locale = Locale.ENGLISH;
+					} else {
+						locale = Locale.ENGLISH;
+					}
+				} else {
+					String option = prefs.getString("SharedPreferences_language", "jp");
+					LanguageCommon languageCommon = new LanguageCommon();
+					locale = languageCommon.intToLocale(Integer.valueOf(option));
+				}
+
+				
+				PanatermApplication.appLanguageFlag = locale.getLanguage();
+				
+				Resources res = getResources();
+				Configuration config = res.getConfiguration();
+				
+				if (!config.locale.getLanguage().equals(PanatermApplication.appLanguageFlag)){
+				
+					PanatermApplication.osLanguageFlag = locale.getLanguage();
+					config.locale = locale;
+					DisplayMetrics dm = res.getDisplayMetrics();
+					res.updateConfiguration(config, dm);
+				}
